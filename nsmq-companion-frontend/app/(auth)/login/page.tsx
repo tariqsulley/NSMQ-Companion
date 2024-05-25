d06b0f0575b189ccc1c9db150d0f3d3a2059f0e0
tariqsulley <tariqsulley3c@gmail.com>
2024-05-25 19:41:08 +0000

    initial setup

---
+++ nsmq-companion-frontend/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import ThemeSwitch from "@/app/components/Theme/ThemeSwitch"
+
+export default function LoginPage() {
+    return (
+        <div>
+            <ThemeSwitch />
+            Login
+        </div>
+    )
+}
--- nsmq-companion-frontend/app/layout.tsx
+++ nsmq-companion-frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
