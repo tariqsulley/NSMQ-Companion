f8beb66adc87f93e9d1dcc323d3c715ef3d6812d
tariqsulley <tariqsulley3c@gmail.com>
2024-05-26 21:50:17 +0000

    Sets up sidebar

--- nsmq-companion-frontend/app/(dashboard)/dashboard/page.tsx
+++ nsmq-companion-frontend/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import Sidebar from "@/app/components/Sidebar"
 export default function Dashboard() {
     return (
-        <div>
-            Welcome to the nsmq companion Dashboard
+        <div className="flex h-screen">
+            <Sidebar />
+            <div className="bg-blue-500 sm:ml-[256px] w-full">
+                <p className="mt-[100px]"> Welcome to the nsmq companion Dashboard</p>
+            </div>
         </div>
     )
 }
---
+++ nsmq-companion-frontend/app/components/Sidebar/index.tsx
@@ -0,0 +1,100 @@
+"use client"
+import 'flowbite';
+import { MdOutlineDashboard } from "react-icons/md";
+import { PiStudentDuotone } from "react-icons/pi";
+import { IoPeopleOutline } from "react-icons/io5";
+import { IoAnalyticsOutline } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
+import { SlBookOpen } from "react-icons/sl";
+
+export default function Sidebar() {
+    return (
+        <div>
+            <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                <div className="px-3 py-3 lg:px-5 lg:pl-3">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center justify-start rtl:justify-end">
+                            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                                <span className="sr-only">Open sidebar</span>
+                                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                                </svg>
+                            </button>
+                            <a href="https://flowbite.com" className="flex ms-2 md:me-24">
+                                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
+                                <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">NSMQ Companion</span>
+                            </a>
+                        </div>
+                        <div className="flex items-center">
+                            <div className="flex items-center ms-3">
+                                <div>
+                                    <button type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                                        <span className="sr-only">Open user menu</span>
+                                        <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo" />
+                                    </button>
+                                </div>
+                                <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
+                                    <div className="px-4 py-3" role="none">
+                                        <p className="text-sm text-gray-900 dark:text-white" role="none">
+                                            Tariq Sulley
+                                        </p>
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                            tariqsulley3c@gmail.com
+                                        </p>
+                                    </div>
+                                    <ul className="py-1" role="none">
+                                        <li>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+                <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+                    <ul className="space-y-2 font-medium">
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <MdOutlineDashboard size={25} />
+                                <span className="ms-3">Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            {"facilitator" ?
+                                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+
+                                    <PiStudentDuotone size={25} />
+                                    <span className="flex-1 ms-3 whitespace-nowrap"> Students </span>
+                                </a> : <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <SlBookOpen size={25} />
+                                    <span className="flex-1 ms-3 whitespace-nowrap"> Practice</span>
+                                </a>}
+                        </li>
+                        <li>
+                            {"facilitator" ?
+                                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <IoAnalyticsOutline size={25} />
+                                    <span className="flex-1 ms-3 whitespace-nowrap"> Analytics</span>
+                                </a> : <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <IoPeopleOutline size={25} />
+                                    <span className="flex-1 ms-3 whitespace-nowrap"> Multiplayer</span>
+                                </a>}
+                        </li>
+                        <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+                            <li>
+                                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <IoSettingsOutline size={25} />
+                                    <span className="flex-1 ms-3 whitespace-nowrap">Settings</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </ul>
+                </div>
+            </aside>
+        </div>
+    )
+}
