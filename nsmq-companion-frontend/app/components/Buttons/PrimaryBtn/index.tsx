00e67c83573229f44cc98e870e3ad6bf79a9cf16
tariqsulley <tariqsulley3c@gmail.com>
2024-05-31 20:11:25 +0000

    changes

--- nsmq-companion-frontend/app/components/Buttons/PrimaryBtn/index.tsx
+++ nsmq-companion-frontend/app/components/Buttons/PrimaryBtn/index.tsx
@@ -10,7 +10,7 @@ interface PrimaryBtnProps {
 const PrimaryBtn: React.FC<PrimaryBtnProps> = ({ loading, txt }) => {
     return (
         <button className="flex items-center justify-center mb-2 w-full 
-        rounded-lg border bg-blue-500 p-2 hover:opacity-80 dark:bg-darkBgBtn">
+        rounded-lg border bg-blue-800 p-2 hover:opacity-80 dark:bg-darkBgBtn">
             <p className="text-white font-semibold"> {loading ? <CgSpinner className="animate-spin" size={30} /> : txt} </p>
         </button>
     );
