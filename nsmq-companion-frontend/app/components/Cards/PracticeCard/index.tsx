9945b1941080d42339985bceeef0554da97adcf3
tariqsulley <tariqsulley3c@gmail.com>
2024-05-31 23:27:27 +0000

    changes

--- nsmq-companion-frontend/app/components/Cards/PracticeCard/index.tsx
+++ nsmq-companion-frontend/app/components/Cards/PracticeCard/index.tsx
@@ -19,29 +19,35 @@ export default function PracticeCard({ year, contest_nums }: PracticeCardProps) 
 
     return (
         <div className="rounded-lg bg-white shadow cursor-pointer sm:w-[30%]" onClick={handleClick}>
-            <div className=" p-2 " >
-                <div className="bg-blue-800 rounded-lg m-2 flex items-center justify-center w-[12%] p-2 left-0">
-                    <GiMaterialsScience size={20} className="text-white" />
-                </div>
+            <div className="relative overflow-hidden group">
+                <Image src={nsmq_logo} alt="image"
+                    className="rounded-t-xl transition-transform duration-300 transform group-hover:scale-110" />
             </div>
+            <div className="bg-gray-100">
+                <div className=" p-2 " >
+                    <div className="bg-blue-800 rounded-lg m-2 flex items-center justify-center w-[12%] p-2 left-0">
+                        <GiMaterialsScience size={20} className="text-white" />
+                    </div>
+                </div>
 
-            <div className="p-2">
-                <div>
-                    <p className="font-bold text-lg">Competition Year: {year}</p>
-                    <p className="text-[#475569]">Flex your science and math muscles with NSMQ 2021 questions</p>
-                </div>
-                <div>
-                    <p className="font-semibold">  40 contests </p>
-                </div>
-                <div className=" ">
-                    <p className="text-green-500 bg-green-600 font-semibold
+                <div className="p-2">
+                    <div>
+                        <p className="font-bold text-lg">Competition Year: {year}</p>
+                        <p className="text-[#475569]">Flex your science and math muscles with NSMQ 2021 questions</p>
+                    </div>
+                    <div>
+                        <p className="font-semibold">  40 contests </p>
+                    </div>
+                    <div className=" ">
+                        <p className="text-green-500 bg-green-600 font-semibold
                     bg-opacity-10 rounded-full px-2 mt-2 "> ● 2 complete </p>
-                </div>
+                    </div>
 
-                <button className="flex shadow justify-center items-center px-6 py-2 gap-1 bg-gray-100 rounded-lg mt-5">
-                    <p className="font-bold">View</p>
-                    <LuArrowUpRightSquare size={20} />
-                </button>
+                    <button className="flex shadow justify-center items-center px-6 py-2 gap-1 bg-gray-100 rounded-lg mt-5">
+                        <p className="font-bold">View</p>
+                        <LuArrowUpRightSquare size={20} />
+                    </button>
+                </div>
             </div>
         </div>
     );
