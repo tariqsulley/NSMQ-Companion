17c0acaaab4f07062954bdc35521cacbc0210ca1
tariqsulley <tariqsulley3c@gmail.com>
2024-06-04 19:14:52 +0000

    uploaded json for remaining rounds of contest 40(2021)

--- nsmq-companion-frontend/app/(dashboard)/dashboard/multiplayer/page.tsx
+++ nsmq-companion-frontend/app/(dashboard)/dashboard/multiplayer/page.tsx
@@ -12,63 +12,12 @@ export default function MultiplayerPage() {
     const [waitingRoomStatus, setWaitingRoomStatus] = useState<'idle' | 'searching' | 'paired'>('idle');
     const [pairedStudent, setPairedStudent] = useState<string | null>(null);
     const { Data } = useAuth()
-
     const joinWaitingRoom = () => {
-        if (socket) {
-            // Send a request to join the waiting room
-            socket.emit('joinWaitingRoom', Data?.uuid);
-            setWaitingRoomStatus('searching');
-        }
+
     };
 
-    useEffect(() => {
-        if (socket) {
-            socket.on('connect', () => {
-                console.log('Connected to server');
-            });
 
-            socket.on('waitingRoomJoined', () => {
-                console.log('Joined the waiting room');
-                setWaitingRoomStatus('searching');
-            });
 
-            socket.on('pairedWithStudent', (studentName) => {
-                console.log(`Paired with ${studentName}`);
-                setPairedStudent(studentName);
-                setWaitingRoomStatus('paired');
-            });
-        }
-    }, [socket]);
-
-    // useEffect((): any => {
-    //     // Connect to the Socket.IO server
-    //     const newSocket = io('http://localhost:8000/ws'); // Update the URL to include '/ws'
-    //     setSocket(newSocket);
-    //     socketRef.current = newSocket;
-
-    //     // Clean up the socket connection on component unmount
-    //     return () => newSocket.disconnect();
-    // }, []);
-
-    useEffect(() => {
-        if (socket) {
-            // Listen for events from the server
-            socket.on('connect', () => {
-                console.log('Connected to server');
-            });
-
-            socket.on('message', (data) => {
-                console.log('Received message:', data);
-            });
-        }
-    }, [socket]);
-
-    const sendMessage = () => {
-        if (socket) {
-            // Send a message to the server
-            socket.emit('message', 'Hello, server!');
-        }
-    };
 
     return (
         <div className="flex h-screen">
