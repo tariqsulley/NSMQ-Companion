31c345ad91acda0850356e3c7055862881dafd44
tariqsulley <tariqsulley3c@gmail.com>
2024-06-07 01:25:05 +0000

    changes

--- nsmq-companion-frontend/app/(dashboard)/dashboard/champion-challenge/page.tsx
+++ nsmq-companion-frontend/app/(dashboard)/dashboard/champion-challenge/page.tsx
@@ -5,7 +5,7 @@ export default function ChampionChallenge() {
     return (
         <div className="flex min-h-screen">
             <Sidebar />
-            <div className="bg-bgMain sm:ml-[256px] w-full flex flex-col items-center">
+            <div className="bg-bgMain dark:bg-darkBgLight sm:ml-[256px] w-full flex flex-col items-center">
                 <div className="mt-[100px] w-[95%] flex flex-col items-center justify-center">
                     <p>Do you have what it takes to be crowned grand champion of the national science and maths quiz?</p>
                     <p>Test your prowess against past champions</p>
--- nsmq-companion-frontend/app/components/Views/ChampionChallengeView/index.tsx
+++ nsmq-companion-frontend/app/components/Views/ChampionChallengeView/index.tsx
@@ -1,6 +1,6 @@
 "use client"
-import winner_2021 from "../../../../public/images/2021-winner.jpg"
-import winner_2020 from "../../../../public/images/2020-winner.png"
+import winner_2021 from "../../../../public/images/prempeh.jpg"
+import winner_2020 from "../../../../public/images/presec.jpg"
 import winner_2019 from "../../../../public/images/2019-winner.jpg"
 import winner_2018 from "../../../../public/images/2018-winner.png"
 import winner_2017 from "../../../../public/images/2017-winner.png"
@@ -10,12 +10,13 @@ import { FaStar } from "react-icons/fa6";
 import Image from "next/image"
 export default function ChampionChallengeView() {
     return (
-        <div className="flex flex-col w-full  ">
+        <div className="flex flex-col w-full bg-gray-200 dark:bg-gray-700 ">
 
             <div>
-                <div className="bg-white shadow">
+                <div className="bg-white dark:bg-darkBgDeep shadow">
                     <div className="bg p-2 shadow  relative overflow-hidden group">
-                        <Image src={winner_2021} alt="image" className="h-[120px]  rounded-t-xl transition-transform duration-300 transform group-hover:scale-110 " />
+                        <Image src={winner_2021} alt="image" className=" w-[20%] rounded-t-xl
+                         transition-transform duration-300 transform group-hover:scale-110 " />
                     </div>
                     <div className="mx-2">
                         <p className="font-bold text-xl">2021</p>
@@ -46,9 +47,9 @@ export default function ChampionChallengeView() {
                 </div>
             </div>
             <div>
-                <div className="bg-white shadow">
+                <div className="bg-white dark:bg-darkBgDeep shadow">
                     <div className="bg p-2 shadow  relative overflow-hidden group">
-                        <Image src={winner_2020} alt="image" className="h-[120px]  rounded-t-xl transition-transform duration-300 transform group-hover:scale-110 " />
+                        <Image src={winner_2020} alt="image" className="w-[15%] rounded-t-xl transition-transform duration-300 transform group-hover:scale-110 " />
                     </div>
                     <div className="mx-2">
                         <p className="font-bold text-xl">2020</p>
