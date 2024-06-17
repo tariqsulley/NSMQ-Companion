c11aae2a22c9e0efdf35c37b8f644c37eb0b6ee9
tariqsulley <tariqsulley3c@gmail.com>
2024-06-17 14:22:27 +0000

    changes

--- nsmq-companion-frontend/app/utils/NSMQContests.js
+++ nsmq-companion-frontend/app/utils/NSMQContests.js
@@ -8,63 +8,9 @@ const ContestData = [
     {
         "year": "2020",
         "file": "",
-        "contest_nums": "3",
-        "sub_text": "Experience the exhilarating thrill of NSMQ 2021 all over again in vivid detail."
-    },
-    // {
-    //     "year": "2019",
-    //     "file": "",
-    //     "contest_nums": "40",
-    //     "sub_text": "Challenge yourself with questions from NSMQ 2019"
-    // },
-    // {
-    //     "year": "2018",
-    //     "file": "",
-    //     "contest_nums": "40",
-    //     "sub_text": "Relive the excitement of NSMQ 2018"
-    // },
-    // {
-    //     "year": "2017",
-    //     "file": "",
-    //     "contest_nums": "40",
-    //     "sub_text": "Uncover the mysteries that baffled NSMQ 2017 participants"
-    // },
-    // {
-    //     "year": "2016",
-    //     "file": "",
-    //     "contest_nums": "40",
-    //     "sub_text": "Immerse yourself in the cerebral arena of NSMQ 2016"
-    // },
-    // {
-    //     "year": "2015",
-    //     "file": "",
-    //     "contest_nums": "67",
-    //     "sub_text": "Cerebral warrior, assemble! NSMQ 2015 awaits"
-    // },
-    // {
-    //     "year": "2014",
-    //     "file": "",
-    //     "contest_nums": "67",
-    //     "sub_text": "Conquer the challenges that stumped contestants in NSMQ 2014"
-    // },
-    // {
-    //     "year": "2013",
-    //     "file": "",
-    //     "contest_nums": "40",
-    //     "sub_text": "Unlock the secrets of NSMQ 2013's toughest questions"
-    // },
-    // {
-    //     "year": "2012",
-    //     "file": "",
-    //     "contest_nums": "37",
-    //     "sub_text": "Relive the glory of NSMQ 2012 - Ghana's intellectual showdown"
-    // },
-    // {
-    //     "year": "2009",
-    //     "file": "",
-    //     "contest_nums": "22",
-    //     "sub_text": "Defy the boundaries of intelligence with NSMQ 2021's questions"
-    // },
+        "contest_nums": "1",
+        "sub_text": "Experience the exhilarating thrill of NSMQ 2020 all over again in vivid detail."
+    }
 ]
 
 export default ContestData
